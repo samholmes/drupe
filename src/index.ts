07aec4ad54216060285621f516fc4ec75b4a1b7d
Sam Holmes <sam.lb.holmes@gmail.com>
2025-11-07 08:43:24 -0800

    Add jsr.json

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { matchesSelector } from 'pouchdb-selector-core'
 import type { MangoSelector } from './types'
 
-export const drupe = (selector: MangoSelector) => (subject: unknown) => {
+export const drupe = (selector: MangoSelector) => (subject: unknown): boolean => {
   return matchesSelector(subject, selector)
 }
 
